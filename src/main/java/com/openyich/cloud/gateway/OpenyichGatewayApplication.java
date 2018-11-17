a5456ced32a8912ccc2cc065964ba63bc518fe93
zhycn <qzhservice@gmail.com>
2018-11-17 21:58:03 +0800

    Initial commit

---
+++ src/main/java/com/openyich/cloud/gateway/OpenyichGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.openyich.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenyichGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenyichGatewayApplication.class, args);
+  }
+}
