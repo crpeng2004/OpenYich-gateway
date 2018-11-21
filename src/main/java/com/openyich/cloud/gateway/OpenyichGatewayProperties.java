51e2dca3bfa8e17fbf4ae14897b426b363a2374e
zhycn <qzhservice@gmail.com>
2018-11-21 18:12:12 +0800

    Add JWT TOKEN

--- src/main/java/com/openyich/cloud/gateway/OpenyichGatewayProperties.java
+++ src/main/java/com/openyich/cloud/gateway/OpenyichGatewayProperties.java
@@ -7,4 +7,63 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "openyich.gateway")
 public class OpenyichGatewayProperties {
 
+  private final Security security = new Security();
+
+  public Security getSecurity() {
+    return security;
+  }
+
+  public static class Security {
+
+    private final Jwt jwt = new Jwt();
+
+    public Jwt getJwt() {
+      return jwt;
+    }
+
+    public static class Jwt {
+
+      private String secret;
+
+      private String base64Secret;
+
+      private long tokenValidityInSeconds = 1800; // 0.5 hour
+
+      private long tokenValidityInSecondsForRememberMe = 2592000; // 30 days
+
+      public String getSecret() {
+        return secret;
+      }
+
+      public void setSecret(String secret) {
+        this.secret = secret;
+      }
+
+      public String getBase64Secret() {
+        return base64Secret;
+      }
+
+      public void setBase64Secret(String base64Secret) {
+        this.base64Secret = base64Secret;
+      }
+
+      public long getTokenValidityInSeconds() {
+        return tokenValidityInSeconds;
+      }
+
+      public void setTokenValidityInSeconds(long tokenValidityInSeconds) {
+        this.tokenValidityInSeconds = tokenValidityInSeconds;
+      }
+
+      public long getTokenValidityInSecondsForRememberMe() {
+        return tokenValidityInSecondsForRememberMe;
+      }
+
+      public void setTokenValidityInSecondsForRememberMe(long tokenValidityInSecondsForRememberMe) {
+        this.tokenValidityInSecondsForRememberMe = tokenValidityInSecondsForRememberMe;
+      }
+    }
+  }
+
+
 }
