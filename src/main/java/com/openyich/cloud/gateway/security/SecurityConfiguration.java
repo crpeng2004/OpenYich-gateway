d33dea438b6569b8d5074bde850c99707357a135
zhycn <qzhservice@gmail.com>
2018-11-21 20:14:45 +0800

    Format code

--- src/main/java/com/openyich/cloud/gateway/security/SecurityConfiguration.java
+++ src/main/java/com/openyich/cloud/gateway/security/SecurityConfiguration.java
@@ -26,93 +26,82 @@ import reactor.core.publisher.Mono;
 public class SecurityConfiguration {
 
   /**
-   * A custom UserDetailsService to provide quick user rights for Spring Security,
-   * more formal implementations may be added as separated files and annotated as
-   * a Spring stereotype.
+   * A custom UserDetailsService to provide quick user rights for Spring Security, more formal
+   * implementations may be added as separated files and annotated as a Spring stereotype.
    *
    * @return MapReactiveUserDetailsService an InMemory implementation of user details
    */
+  @SuppressWarnings("deprecation")
   @Bean
   public MapReactiveUserDetailsService userDetailsRepository() {
-      UserDetails user = User.withDefaultPasswordEncoder()
-              .username("user")
-              .password("user")
-              .roles("USER", "ADMIN")
-              .build();
-      return new MapReactiveUserDetailsService(user);
+    UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user")
+        .roles("USER", "ADMIN").build();
+    return new MapReactiveUserDetailsService(user);
   }
 
   /**
-   * For Spring Security webflux, a chain of filters will provide user authentication
-   * and authorization, we add custom filters to enable JWT token approach.
+   * For Spring Security webflux, a chain of filters will provide user authentication and
+   * authorization, we add custom filters to enable JWT token approach.
    *
-   * @param http An initial object to build common filter scenarios.
-   *             Customized filters are added here.
-   * @return SecurityWebFilterChain A filter chain for web exchanges that will
-   * provide security
+   * @param http An initial object to build common filter scenarios. Customized filters are added
+   *        here.
+   * @return SecurityWebFilterChain A filter chain for web exchanges that will provide security
    **/
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
-      http
-              .authorizeExchange()
-                  .pathMatchers("/login", "/")
-                  .authenticated()
-              .and()
-                  .addFilterAt(basicAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
-                     .authorizeExchange()
-                  .pathMatchers("/api/**")
-                  .authenticated()
-              .and()
-                  .addFilterAt(bearerAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
+    http.authorizeExchange().pathMatchers("/login", "/").authenticated().and()
+        .addFilterAt(basicAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
+        .authorizeExchange().pathMatchers("/api/**").authenticated().and()
+        .addFilterAt(bearerAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
 
-      return http.build();
+    return http.build();
   }
 
   /**
-   * Use the already implemented logic in  AuthenticationWebFilter and set a custom
-   * SuccessHandler that will return a JWT when a user is authenticated with user/password
-   * Create an AuthenticationManager using the UserDetailsService defined above
+   * Use the already implemented logic in AuthenticationWebFilter and set a custom SuccessHandler
+   * that will return a JWT when a user is authenticated with user/password Create an
+   * AuthenticationManager using the UserDetailsService defined above
    *
    * @return AuthenticationWebFilter
    */
-  private AuthenticationWebFilter basicAuthenticationFilter(){
-      UserDetailsRepositoryReactiveAuthenticationManager authManager;
-      AuthenticationWebFilter basicAuthenticationFilter;
-      ServerAuthenticationSuccessHandler successHandler;
+  private AuthenticationWebFilter basicAuthenticationFilter() {
+    UserDetailsRepositoryReactiveAuthenticationManager authManager;
+    AuthenticationWebFilter basicAuthenticationFilter;
+    ServerAuthenticationSuccessHandler successHandler;
 
-      authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository());
-      successHandler = new  BasicAuthenticationSuccessHandler();
+    authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository());
+    successHandler = new BasicAuthenticationSuccessHandler();
 
-      basicAuthenticationFilter = new AuthenticationWebFilter(authManager);
-      basicAuthenticationFilter.setAuthenticationSuccessHandler(successHandler);
+    basicAuthenticationFilter = new AuthenticationWebFilter(authManager);
+    basicAuthenticationFilter.setAuthenticationSuccessHandler(successHandler);
 
-      return basicAuthenticationFilter;
+    return basicAuthenticationFilter;
 
   }
 
   /**
-   * Use the already implemented logic by AuthenticationWebFilter and set a custom
-   * converter that will handle requests containing a Bearer token inside
-   * the HTTP Authorization header.
-   * Set a dummy authentication manager to this filter, it's not needed because
-   * the converter handles this.
+   * Use the already implemented logic by AuthenticationWebFilter and set a custom converter that
+   * will handle requests containing a Bearer token inside the HTTP Authorization header. Set a
+   * dummy authentication manager to this filter, it's not needed because the converter handles
+   * this.
    *
    * @return bearerAuthenticationFilter that will authorize requests containing a JWT
    */
-  private AuthenticationWebFilter bearerAuthenticationFilter(){
-      AuthenticationWebFilter bearerAuthenticationFilter;
-      Function<ServerWebExchange, Mono<Authentication>> bearerConverter;
-      ReactiveAuthenticationManager authManager;
+  private AuthenticationWebFilter bearerAuthenticationFilter() {
+    AuthenticationWebFilter bearerAuthenticationFilter;
+    Function<ServerWebExchange, Mono<Authentication>> bearerConverter;
+    ReactiveAuthenticationManager authManager;
 
-      authManager  = new BearerTokenReactiveAuthenticationManager();
-      bearerAuthenticationFilter = new AuthenticationWebFilter(authManager);
-      bearerConverter = new ServerHttpBearerAuthenticationConverter();
+    authManager = new BearerTokenReactiveAuthenticationManager();
+    bearerAuthenticationFilter = new AuthenticationWebFilter(authManager);
+    bearerConverter = new ServerHttpBearerAuthenticationConverter();
 
-      bearerAuthenticationFilter.setAuthenticationConverter(bearerConverter);
-      bearerAuthenticationFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/api/**"));
+    bearerAuthenticationFilter.setAuthenticationConverter(bearerConverter);
+    bearerAuthenticationFilter
+        .setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/api/**"));
 
-      return bearerAuthenticationFilter;
+    return bearerAuthenticationFilter;
   }
-  
+
 }
--- src/main/java/com/openyich/cloud/gateway/security/bearer/ServerHttpBearerAuthenticationConverter.java
+++ src/main/java/com/openyich/cloud/gateway/security/bearer/ServerHttpBearerAuthenticationConverter.java
@@ -1,22 +1,3 @@
-/*
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package com.openyich.cloud.gateway.security.bearer;
 
 import java.util.function.Function;
@@ -32,31 +13,32 @@ import com.openyich.cloud.gateway.security.jwt.UsernamePasswordAuthenticationBea
 import reactor.core.publisher.Mono;
 
 /**
- * This converter extracts a bearer token from a WebExchange and
- * returns an Authentication object if the JWT token is valid.
- * Validity means is well formed and signature is correct
+ * This converter extracts a bearer token from a WebExchange and returns an Authentication object if
+ * the JWT token is valid. Validity means is well formed and signature is correct
  */
-public class ServerHttpBearerAuthenticationConverter implements Function<ServerWebExchange, Mono<Authentication>> {
+public class ServerHttpBearerAuthenticationConverter
+    implements
+      Function<ServerWebExchange, Mono<Authentication>> {
 
-    private static final String BEARER = "Bearer ";
-    private static final Predicate<String> matchBearerLength = authValue -> authValue.length() > BEARER.length();
-    private static final Function<String,Mono<String>> isolateBearerValue = authValue -> Mono.justOrEmpty(authValue.substring(BEARER.length()));
+  private static final String BEARER = "Bearer ";
+  private static final Predicate<String> matchBearerLength =
+      authValue -> authValue.length() > BEARER.length();
+  private static final Function<String, Mono<String>> isolateBearerValue =
+      authValue -> Mono.justOrEmpty(authValue.substring(BEARER.length()));
 
-    private  JWTCustomVerifier jwtVerifier = new JWTCustomVerifier();
-    /**
-     * Apply this function to the current WebExchange, an Authentication object
-     * is returned when completed.
-     *
-     * @param serverWebExchange
-     * @return
-     */
-    @Override
-    public Mono<Authentication> apply(ServerWebExchange serverWebExchange) {
-        return Mono.justOrEmpty(serverWebExchange)
-                .flatMap(AuthorizationHeaderPayload::extract)
-                   .filter(matchBearerLength)
-                .flatMap(isolateBearerValue)
-                .flatMap(jwtVerifier::check)
-                .flatMap(UsernamePasswordAuthenticationBearer::create).log();
-    }
+  private JWTCustomVerifier jwtVerifier = new JWTCustomVerifier();
+
+  /**
+   * Apply this function to the current WebExchange, an Authentication object is returned when
+   * completed.
+   *
+   * @param serverWebExchange
+   * @return
+   */
+  @Override
+  public Mono<Authentication> apply(ServerWebExchange serverWebExchange) {
+    return Mono.justOrEmpty(serverWebExchange).flatMap(AuthorizationHeaderPayload::extract)
+        .filter(matchBearerLength).flatMap(isolateBearerValue).flatMap(jwtVerifier::check)
+        .flatMap(UsernamePasswordAuthenticationBearer::create).log();
+  }
 }
